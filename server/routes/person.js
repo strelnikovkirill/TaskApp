908472eb96a9b4370b1d0eea56b0ce8e55631822
Nikita Buzanov <buzanovn@gmail.com>
2019-05-31 11:57:10 +0300

    Person addition logic

--- server/routes/person.js
+++ server/routes/person.js
@@ -1,21 +1,30 @@
 const express = require('express');
 const personRoutes = express.Router();
-
+const sha256 = require('../utils/hash');
 let Person = require('../models/person');
 
 personRoutes.route('/add').post(function (req, res) {
-  let person = new Person(req.body);
-  person.save()
+  let reqPerson = new Person(req.body);
+  Person.findOne({login: req.login}, function (err, person) {
+    if (person) {
+      console.log(person);
+      res.status(500).json({'success': false, 'message': 'A person with such login already exists'})
+    } else {
+      reqPerson.token = sha256(req.login, "s0me@ver4#!#secure=+=sa1t");
+      reqPerson.save()
         .then(person => {
-          res.status(200).json({'person': 'Person is added successfully'});
+          res.status(200).json({'success': true, token: person.token});
         })
         .catch(err => {
+          console.log(err);
           res.status(400).send('Unable to save to database!');
-        });
+        })
+    }
+  })
 });
 
 personRoutes.route('/').get(function (req, res) {
-  Person.find(function(err, person) {
+  Person.find(function (err, person) {
     if (err) console.log(err);
     else res.json(person);
   });
@@ -29,7 +38,7 @@ personRoutes.route('/edit/:id').get(function (req, res) {
 });
 
 personRoutes.route('/update/:id').post(function (req, res) {
-  Person.findById(req.params.id, function(err, person) {
+  Person.findById(req.params.id, function (err, person) {
     if (!person)
       res.status(404).send('Data is not found!');
     else {
@@ -38,17 +47,17 @@ personRoutes.route('/update/:id').post(function (req, res) {
       person.login = req.body.login;
       person.password = req.body.password;
       person.save().then(person => {
-              res.json('Update complete!');
-            })
-            .catch(err => {
-              res.status(400).send('Unable to update the database!');
-            });
+        res.json('Update complete!');
+      })
+        .catch(err => {
+          res.status(400).send('Unable to update the database!');
+        });
     }
   });
 });
 
 personRoutes.route('/delete/:id').get(function (req, res) {
-  Person.findByIdAndRemove({ _id: req.params.id }, function(err, person) {
+  Person.findByIdAndRemove({_id: req.params.id}, function (err, person) {
     if (err) res.json(err);
     else res.json('Person is successfully removed!');
   });
