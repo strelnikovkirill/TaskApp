bf15ff7bb00831d6d87e2d5e1b46658b8611f402
Nikita Buzanov <buzanovn@gmail.com>
2019-05-31 11:58:57 +0300

    Support for auth routes in server

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 const config = require('./DB.js');
 const taskRoute = require('./routes/task');
 const personRoute = require('./routes/person');
+const authRoute = require('./routes/auth');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB, { useNewUrlParser: true }).then(
@@ -19,6 +20,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/task', taskRoute);
 app.use('/person', personRoute);
+app.use('/auth', authRoute);
 app.listen(PORT, function() {
   console.log('Server is running on Port: ', PORT);
 });
