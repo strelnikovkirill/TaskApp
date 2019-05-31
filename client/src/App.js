62f424b0410e1838d69dfdeda94aef8257ab1b8f
Nikita Buzanov <buzanovn@gmail.com>
2019-05-31 11:59:20 +0300

    Added ability to logout

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,77 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 import Create from './components/task/create';
 import Edit from './components/task/edit';
 import List from './components/task/list';
-import Auth from './components/auth';
+import Register from './components/register';
+import AuthPage from './components/authpage';
 import Profile from './components/profile';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.logOut = this.logOut.bind(this);
+  }
+
+  logOut() {
+    ['token', 'login'].forEach(e => localStorage.removeItem(e));
+    window.location = '/';
+  }
+
   render() {
-    return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to={'/'} className="navbar-brand">Task app</Link>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav mr-auto">
-                <li className="nav-item">
-                  <Link to={'/list'} className="nav-link">My tasks</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/create'} className="nav-link">Add task</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/profile'} className="nav-link">Profile</Link>
-                </li>
-                <li className="nav-item">
-                  <Link to={'/auth'} className="nav-link">Auth</Link>
-                </li>
-              </ul>
+    let token = localStorage.getItem('token');
+    if (token != null) {
+      return (
+        <Router>
+          <div className="container-fluid">
+            <nav className="navbar navbar-expand-sm navbar-dark bg-primary">
+              <Link to={'/'} className="navbar-brand">Task app</Link>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                  <li className="nav-item">
+                    <Link to={'/list'} className="nav-link">My tasks</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to={'/create'} className="nav-link">Add task</Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link to={'/profile'} className="nav-link">Profile</Link>
+                  </li>
+                </ul>
+              <ul className="navbar-nav">
+                  <li className="nav-item">
+                    Logged in as {localStorage.getItem('login')}
+                  </li>
+                  <li className="nav-item">
+                    <button className="btn btn-outline-primary" onClick={this.logOut}>Log out</button>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <br/>
+            <div className="container">
+              <Switch>
+                <Route exact path='/create' component={Create}/>
+                <Route path='/edit/:id' component={Edit}/>
+                <Route path='/list' component={List}/>
+                <Route path='/auth' component={Register}/>
+                <Route path='/profile' component={Profile}/>
+              </Switch>
             </div>
-          </nav>
-          <br/>
-          <Switch>
-            <Route exact path='/create' component={ Create }/>
-            <Route path='/edit/:id' component={ Edit }/>
-            <Route path='/list' component={ List }/>
-            <Route path='/auth' component={ Auth }/>
-            <Route path='/profile' component={ Profile }/>
-          </Switch>
+          </div>
+        </Router>
+      );
+    } else {
+      //TODO: check token on backend
+      return (
+        <div>
+          <AuthPage/>
+          <Register/>
         </div>
-      </Router>
-    );
+      )
+    }
   }
 }
 
